23eed749a456f777bdb25e34fbef9b14c7c25063
Angelinne <linneange3@gmail.com>
2025-06-11 22:41:25 -0300

    makefile

---
+++ include/constants.h
@@ -0,0 +1,17 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const int LARGURA_TELA = 800;
+const int ALTURA_TELA = 600;
+
+const int GRAVIDADE = 1;
+const int FORCA_PULO = -12;
+
+const int ESPACO_ENTRE_TUBOS = 200;
+const int LARGURA_TUBO = 110;
+const int ALTURA_TUBO = 600;
+const int VELOCIDADE_TUBO = 2;
+
+const int VELOCIDADE_FUNDO = 2;
+
+#endif
